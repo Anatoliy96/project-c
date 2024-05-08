389243cdc7930db4572c96fef081ce93048ff123
Anatoliy <leaved@abv.bg>
2024-05-08 16:53:01 +0300

    text size

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -5,7 +5,7 @@ const Form = () => {
     <section className='bg-customGray'>
         <div className='flex flex-col items-center w-full h-full pb-10'>
         <div className='text-white font-space-grotesk lg:w-[50%] w-full mx-auto p-5'>
-            <h1 className='font-semibold lg:text-[72px] text-5xl mt-10'>Готово ли сте да работим заедно?</h1>
+            <h1 className='font-semibold lg:text-[72px] text-4xl mt-10'>Готово ли сте да работим заедно?</h1>
             <p>Оставете вашият телефон и ние ще се свържем с вас, за да отговорим на всички въпроси</p>
         </div>
         <div className='flex p-4'>
