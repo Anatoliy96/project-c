48098d4ac86a9b880dc12ca0586093af3c66c95a
Anatoliy <leaved@abv.bg>
2024-05-08 16:45:54 +0300

    large screen for mobile

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <section>
         <div className='flex flex-col w-[90%] mx-auto text-center'>
             <div className='w-[80%] mx-auto p-10 text-customBlack'>
-            <h1 className='text-space-grotesk text-black font-semibold text-[72px] w-[80%] mx-auto 
+            <h1 className='text-space-grotesk text-black font-semibold lg:text-[72px] w-[80%] mx-auto 
             sm:text-4xl md-mt-[70px] md:leading-[108%] md:-tracking-[4px] lg:w-[50%] lg:text-5xl lg:pt-10'>Гъвкави решения за сухопътен транспорт за всеки клиент</h1>
             </div>
             <div className='flex justify-center'>
