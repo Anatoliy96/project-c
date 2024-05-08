90d79183bd97ba0d7ad7bb03bc8958a13307f41f
Anatoliy <leaved@abv.bg>
2024-05-08 16:55:10 +0300

    width and padding changes

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,9 +4,9 @@ const Header = () => {
   return (
     <section>
         <div className='flex flex-col w-[90%] mx-auto text-center'>
-            <div className='w-[80%] mx-auto p-10 text-customBlack'>
-            <h1 className='text-space-grotesk text-black font-semibold lg:text-[72px] w-[80%] mx-auto 
-             text-4xl md-mt-[70px] md:leading-[108%] md:-tracking-[4px] lg:w-[50%] lg:pt-10'>Гъвкави решения за сухопътен транспорт за всеки клиент</h1>
+            <div className='lg:w-[80%] w-full mx-auto p-10 text-customBlack'>
+            <h1 className='text-space-grotesk text-black font-semibold lg:text-[72px] w-[80%] w- full mx-auto 
+             text-4xl md-mt-[70px] md:leading-[108%] md:-tracking-[4px] lg:w-[50%] lg:pt-10 pb-2'>Гъвкави решения за сухопътен транспорт за всеки клиент</h1>
             </div>
             <div className='flex justify-center'>
             <img src='hero-image.png' width={969} height={690}></img>                    
