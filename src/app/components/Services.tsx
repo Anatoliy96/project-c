eb6e4e2dda8c008e021d4e045d8d792ee7cc04c2
Anatoliy <leaved@abv.bg>
2024-05-09 18:55:25 +0300

    fix services spacing

--- src/app/components/Services.tsx
+++ src/app/components/Services.tsx
@@ -3,26 +3,26 @@
 const Services = () => {
     return (
       <section>
-          <div className="flex justify-start p-10 items-center">
+          <div className="flex justify-start p-8 lg:pt-10 lg:pl-10 items-center">
               <h1 className="font-space-grotesk font-semibold text-5xl text-customBlack p-5 lg:mr-[1000px] mr-[100px]">Други услуги</h1>
           </div>
           <div className="flex flex-col lg:flex-row gap-4 p-5">
-              <div className="flex flex-col gap-1 lg:w-[30%]  items-center justify-center">
+              <div className="flex flex-col gap-1 lg:w-[35%]  items-center justify-center">
                   <img src="opasni-tovari.png" alt="" className="rounded-xl hover:scale-105 duration-300"/>
                   <p className="text-customBlue text-2xl font-semibold p-5">Опасни товари
                       (ADR)
                   </p>
               </div>
   
-              <div className="flex flex-col gap-1 lg:w-[30%] items-center justify-center">
+              <div className="flex flex-col gap-1 lg:w-[35%] items-center justify-center">
                   <img src="hladilen-transport.png" alt="" className="rounded-xl hover:scale-105 duration-300" />
                   <p className="text-customBlue text-2xl font-semibold p-5 ">Хладилен транспорт
                   </p>
               </div>
   
-              <div className="flex flex-col gap-1 lg:w-[30%] items-center justify-center">
+              <div className="flex flex-col gap-1 lg:w-[35%] items-center justify-center">
                   <img src="izvyngabaritni-tovari.png" alt="" className="rounded-xl hover:scale-105 duration-300" />
-                  <p className="text-customBlue text-2xl font-semibold p-5 ">Извънгабаритни
+                  <p className="text-customBlue text-2xl font-semibold p-5">Извънгабаритни
                   </p>
               </div>
   
